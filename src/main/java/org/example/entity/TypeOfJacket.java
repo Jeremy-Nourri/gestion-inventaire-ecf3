a271edbc7f2ef50c1e782050675b917190e700aa
Jeremy-Nourri <nourri.jer@gmail.com>
2024-05-30 17:24:44 +0200

    feat: create services and Ihm for Clothing, Sale,SaleLine, Customer

---
+++ src/main/java/org/example/entity/TypeOfJacket.java
@@ -0,0 +1,7 @@
+package org.example.entity;
+
+public enum TypeOfJacket {
+    BLAZER,
+    COAT,
+    WINDBREAKER,
+}
---
+++ src/main/java/org/example/entity/TypeOfSleeve.java
@@ -0,0 +1,8 @@
+package org.example.entity;
+
+public enum TypeOfSleeve {
+    SHORT,
+    LONG,
+    SLEEVELESS,
+
+}
